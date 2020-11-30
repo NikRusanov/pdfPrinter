c2751ce30623141854fdf5bed4b0dad39369c694
Nikita Rusanov <kenya.rusanov@gmail.com>
2020-11-30 12:14:11 +0300

    fix

--- src/main/java/com/work/printerapp/App.java
+++ src/main/java/com/work/printerapp/App.java
@@ -1,22 +1,14 @@
 package com.work.printerapp;
 
 
-
 import com.work.printerapp.converters.ConverterToJPEG;
-import com.work.printerapp.exceptions.EmptyArgumentException;
 
-import javax.imageio.ImageIO;
-import java.awt.print.PrinterException;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.ForkJoinTask;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -24,7 +16,7 @@ import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 public class App {
     Printer printer = new Printer();
-    App(ArrayList<String> sourcePaths, String targetPath) throws IOException {
+    App(ArrayList<String> sourcePaths, String targetPath) {
         System.out.print("Enter your destination folder where save image \n");
 
         Path tempFile = Path.of(sourcePaths.get(0) + "_ARHIVE");
@@ -33,7 +25,7 @@ public class App {
             catch (Exception e ) { e.printStackTrace(); }
         }
         System.out.print("Enter your selected pdf files name with source folder \n");
-        List<Path> pdfsInDir = null;
+        List<Path> pdfsInDir;
         for(var sourcePath : sourcePaths) {
             try (Stream<Path> walk = Files.list(Paths.get(sourcePath))) {
                 pdfsInDir = walk.
@@ -43,10 +35,7 @@ public class App {
                                 .endsWith("pdf"))
                         .collect(Collectors
                                 .toCollection(ArrayList::new));
-                ForkJoinPool ioPool = new ForkJoinPool(64);
-                List<Path> finalPdfsInDir = pdfsInDir;
-                ForkJoinTask<?> tasks = ioPool.submit(
-                        () ->          finalPdfsInDir.parallelStream().forEach(path -> {
+               pdfsInDir.parallelStream().forEach(path -> {
                             ConverterToJPEG converter = new ConverterToJPEG(targetPath, path.toFile());
                             converter.convert();
                     try {
@@ -54,14 +43,13 @@ public class App {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                }));
-                tasks.join();
+                });
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        printer.printImages(targetPath);
+      // printer.printImages(targetPath);
     }
 }
 
--- src/main/java/com/work/printerapp/converters/ConverterToJPEG.java
+++ src/main/java/com/work/printerapp/converters/ConverterToJPEG.java
@@ -1,5 +1,6 @@
 package com.work.printerapp.converters;
 
+import org.apache.pdfbox.io.MemoryUsageSetting;
 import org.apache.pdfbox.jbig2.JBIG2ImageReaderSpi;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -11,6 +12,7 @@ import javax.imageio.ImageIO;
 import javax.imageio.spi.IIORegistry;
 import java.awt.image.BufferedImage;
 
+import java.awt.image.MemoryImageSource;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -31,9 +33,9 @@ public class ConverterToJPEG implements ConverterPDF,Runnable {
         this.inputPDF = inputPDF;
     }
 
-public String getImagesFolder() {
+    public String getImagesFolder() {
         return  imagesFolder;
-}
+    }
     public void setDestination(String destination) {
         this.destination = destination;
     }
@@ -41,19 +43,18 @@ public String getImagesFolder() {
     public void  convert()  {
         IIORegistry.getDefaultInstance().registerServiceProvider(new JBIG2ImageReaderSpi());
         if (inputPDF.exists()) {
-            try( PDDocument doc = PDDocument.load(inputPDF)) {
+            try( PDDocument doc = PDDocument.load(inputPDF, MemoryUsageSetting.setupTempFileOnly())) {
                 PDFRenderer renderer = new PDFRenderer(doc);
                 String fileName = inputPDF.getName().replace(".pdf", "");
                 Path savePath = Paths.get(destination +  File.separator + fileName);
 
                 Files.createDirectories(savePath);
                 System.out.println("CONVERTER START.....");
-                int pageCounter = 0 ;
-                for (PDPage page : doc.getPages()) {
-                    String imageName = fileName + "_" + (pageCounter++) + ".jpg";
+                for (int i = 0 ; i < doc.getNumberOfPages(); ++i) {
+                    String imageName = fileName + "_" + i + ".jpg";
                     File fileTemp = new File(savePath.toFile() ,   imageName); // jpg or png
                     if(!fileTemp.exists()) {
-                        BufferedImage image = renderer.renderImageWithDPI(pageCounter, 75, ImageType.RGB);
+                        BufferedImage image = renderer.renderImageWithDPI(i, 100, ImageType.RGB);
                         ImageIO.write(image, "jpg", fileTemp);
                     }
                 }
